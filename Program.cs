5b761d63a41532ee0797b27ae4897b23cacf730d
Andrey-az <andrey_az@mail.ru>
2023-02-13 21:16:11 +1000

    Закончили код

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Введите число a");
+string text_a = Console.ReadLine()!;
+int number_a;
+int.TryParse(text_a , out number_a);
+
+Console.WriteLine("Введите число b");
+string text_b = Console.ReadLine()!;
+int number_b;
+int.TryParse(text_b , out number_b);
+
+int a = number_a;
+int b = number_b;
+int result = number_a;
+
+if (b > a) result = number_b;
+        else result = number_a;
+
+Console.Write("Максимальное число ");
+Console.Write(result);
